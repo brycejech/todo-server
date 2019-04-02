c666f21ab46dbde5779f367e3636427092a76aa8
Bryce Jech <bryce@brycejech.com>
2019-04-02 18:19:34 -0500

    express setup

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express    = require('express'),
+      bodyParser = require('body-parser'),
+      routes     = require('./routes'),
+      conf       = require('./conf');
+
+const server = express();
+
+server.set('case sensitive routing', true);
+// If running behind Nginx proxy
+server.enable('trust proxy');
+
+// Accept form data
+const jsonBody = bodyParser.json(),
+      urlBody  = bodyParser.urlencoded({ extended: true });
+server.use(jsonBody, urlBody);
+
+// Remove "X-Powered-By" header
+server.use((req, res, next) => {
+    res.removeHeader('X-Powered-By');
+    next();
+});
+
+// Mount router
+server.use(routes);
+
+server.listen(conf.server.port, () => {
+    console.log(`Express server listening on ${ conf.server.port }`);
+});
