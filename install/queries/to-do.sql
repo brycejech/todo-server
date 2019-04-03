3487db3fba5e7132ac188d0170d56135ea284da4
Bryce Jech <bryce@brycejech.com>
2019-04-02 22:36:45 -0500

    add complete/description columns

--- install/queries/to-do.sql
+++ install/queries/to-do.sql
@@ -2,10 +2,12 @@
 DROP TABLE IF EXISTS todo_item CASCADE;
 
 CREATE TABLE todo_item(
-    id    serial       NOT NULL PRIMARY KEY,
-    uuid  char(36)     NOT NULL UNIQUE,
-    title varchar(128) NOT NULL,
-    slug  varchar(128) NOT NULL,
+    id          serial       NOT NULL PRIMARY KEY,
+    uuid        char(36)     NOT NULL UNIQUE,
+    title       varchar(128) NOT NULL,
+    slug        varchar(128) NOT NULL,
+    description text,
+    complete    boolean      NOT NULL DEFAULT FALSE,
 
     list  int          NOT NULL REFERENCES todo_list(id),
 
