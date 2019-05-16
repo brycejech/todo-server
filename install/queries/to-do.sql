b824c6b311d09a704a90a54a6bef3fd46bbeb543
Bryce Jech <bryce@brycejech.com>
2019-05-15 20:04:00 -0500

    add meta, completed_on props to item table

--- install/queries/to-do.sql
+++ install/queries/to-do.sql
@@ -4,14 +4,15 @@ DROP TABLE IF EXISTS todo_item CASCADE;
 CREATE TABLE todo_item(
     id          serial       NOT NULL PRIMARY KEY,
     uuid        char(36)     NOT NULL UNIQUE,
+    list        int          NOT NULL REFERENCES todo_list(id),
     title       varchar(128) NOT NULL,
     slug        varchar(128) NOT NULL,
     description text,
+    meta        text,
     complete    boolean      NOT NULL DEFAULT FALSE,
 
-    list  int          NOT NULL REFERENCES todo_list(id),
-
-    due      timestamp with time zone,
-    created  timestamp with time zone DEFAULT (now() at time zone 'utc'),
-    modified timestamp with time zone
+    completed_on timestamp with time zone,
+    due          timestamp with time zone,
+    created      timestamp with time zone DEFAULT (now() at time zone 'utc'),
+    modified     timestamp with time zone
 );
